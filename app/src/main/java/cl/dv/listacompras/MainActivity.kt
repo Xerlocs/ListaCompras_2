7fd59f6c7ab726e4136c3848d73c8d3d29a157e7
Diego <Xerlocs@yahoo.com>
2023-11-21 18:35:43 -0300

    Commin (reiniciando)

--- app/src/main/java/cl/dv/listacompras/MainActivity.kt
+++ app/src/main/java/cl/dv/listacompras/MainActivity.kt
@@ -3,13 +3,31 @@ package cl.dv.listacompras
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_order -> {
+                //val intentSettings = Intent(this, SettingsActivity::class.java)
+                //startActivity(intentSettings)
+                return true
+            }
+            R.id.action_add -> {
+                //val intentAbout = Intent(this, AboutActivity::class.java)
+                //startActivity(intentAbout)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
 
